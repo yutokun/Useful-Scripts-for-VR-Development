19a6fb7798b8703df42a20f3c7e1a6baaf7e9c3d
yutoVR <yuto.fushimi@icloud.com>
2018-09-10 21:13:46 +0900

    APKBuilder を追加しました。

---
+++ APKBuilder.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+
+public class APKBuilder
+{
+	[MenuItem("Build/Build for Store")]
+	public static void Build()
+	{
+		//ファイル名を取得
+		var path = EditorUtility.SaveFolderPanel("ビルドの保存先を指定してください。", "", "");
+		var levels = new[] {"Assets/Scenes/Launch.unity", "Assets/Scenes/Main.unity"};
+
+		//ビルドナンバーをインクリメント
+		++PlayerSettings.Android.bundleVersionCode;
+
+		//ビルド
+		BuildPipeline.BuildPlayer(levels, path + "/Branch-able Story.apk", BuildTarget.Android, BuildOptions.None);
+	}
+}
