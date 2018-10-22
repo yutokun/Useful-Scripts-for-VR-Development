16bdb7e633cb0183ffdedcca34b952f590e4999c
yutoVR <yuto.fushimi@icloud.com>
2018-10-22 22:45:16 +0900

    SmoothFollow をシンプルに改良しました。

--- SmoothFollow.cs
+++ SmoothFollow.cs
@@ -1,14 +1,13 @@
 ﻿using UnityEngine;
-using System.Collections.Generic;
 
 public class SmoothFollow : MonoBehaviour
 {
 	[SerializeField] Transform followTarget;
+	[SerializeField, Range(0f, 1f)] float followFactor = 0.05f;
 
 	[SerializeField, Header("Position")] bool followPosition = true;
-	[SerializeField] int smoothness = 20;
 	[SerializeField] Vector3 positionOffset = Vector3.forward;
-	List<Vector3> parentPositions = new List<Vector3>();
+	Vector3 prevPosition;
 
 	[SerializeField, Header("Rotation")] bool followRotation = true;
 	[SerializeField] Vector3 eulerOffset;
@@ -31,17 +30,8 @@ public class SmoothFollow : MonoBehaviour
 	{
 		if (followPosition == false) return;
 
-		// 位置を加算
-		parentPositions.Add(followTarget.position);
-		if (parentPositions.Count > smoothness) parentPositions.RemoveAt(0);
-
-		// 追従対象の平均位置を計算
-		var pos = Vector3.zero;
-		foreach (var item in parentPositions) pos += item;
-		pos /= parentPositions.Count;
-
-		// 位置を適用
-		transform.position = pos;
+		transform.position = Vector3.Lerp(prevPosition, followTarget.position, followFactor);
+		prevPosition = transform.position;
 		transform.Translate(positionOffset);
 	}
 
@@ -50,14 +40,8 @@ public class SmoothFollow : MonoBehaviour
 		if (followRotation == false) return;
 
 		// 回転を適用
-		transform.rotation = Quaternion.Slerp(prevRotation, followTarget.rotation, 0.05f);
-		transform.Rotate(eulerOffset);
-
+		transform.rotation = Quaternion.Slerp(prevRotation, followTarget.rotation, followFactor);
 		prevRotation = transform.rotation;
-	}
-
-	void OnValidate()
-	{
-		parentPositions = new List<Vector3>(smoothness);
+		transform.Rotate(eulerOffset);
 	}
 }
