5bdd2885d9d93e7286f36b3274b5b2a2e570dab0
yutoVR <yuto.fushimi@icloud.com>
2019-04-01 22:26:12 +0900

    VRObjectPoser に説明を追加

---
+++ Oculus/VRObjectPoser.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// VR Object pose tracker. (The third Oculus Touch)
+/// </summary>
+public class VRObjectPoser : MonoBehaviour
+{
+	[SerializeField] Vector3 positionOffset;
+	[SerializeField] Quaternion rotationOffset;
+
+	void Update()
+	{
+		OVRPose p = OVRPlugin.GetNodePose(OVRPlugin.Node.DeviceObjectZero, OVRPlugin.Step.Render).ToOVRPose();
+
+		if (OVRPlugin.GetNodePositionTracked(OVRPlugin.Node.DeviceObjectZero))
+		{
+			this.transform.position = p.position + positionOffset;
+		}
+
+		if (OVRPlugin.GetNodeOrientationTracked(OVRPlugin.Node.DeviceObjectZero))
+		{
+			this.transform.rotation = p.orientation * rotationOffset;
+		}
+	}
+}
