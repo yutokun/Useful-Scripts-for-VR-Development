891e9ca5969a6a31c633c67dfdcad23ceaa50e58
yutoVR <yuto.fushimi@icloud.com>
2019-04-07 00:54:40 +0900

    PrefabTester がスケール調整に対応しました。

--- PrefabTester.cs
+++ PrefabTester.cs
@@ -10,6 +10,15 @@ public class PrefabTester : MonoBehaviour
 	bool spawned;
 
 	[SerializeField] TextMesh hud;
+	Vector3 initialScale;
+	float scale = 1f;
+
+	float Scale
+	{
+		get => scale;
+		set => scale = Mathf.Clamp(value, 0f, float.MaxValue);
+	}
+
 
 #if OCULUS
 	static bool DownNextButton => OVRInput.GetDown(OVRInput.RawButton.RThumbstickRight);
@@ -17,12 +26,14 @@ public class PrefabTester : MonoBehaviour
 	static bool DownRespawnButton => OVRInput.GetDown(OVRInput.RawButton.A) || OVRInput.GetDown(OVRInput.RawButton.X);
 	static bool DownDeleteButton => OVRInput.GetDown(OVRInput.RawButton.B) || OVRInput.GetDown(OVRInput.RawButton.Y);
 	static bool DownIndexTrigger => OVRInput.GetDown(OVRInput.RawButton.LIndexTrigger) || OVRInput.GetDown(OVRInput.RawButton.RIndexTrigger);
+	static float ScaleChanger => OVRInput.Get(OVRInput.RawAxis2D.RThumbstick).y;
 #else
 	static bool DownNextButton => false;
 	static bool DownPreviousButton => false;
 	static bool DownRespawnButton => false;
 	static bool DownDeleteButton => false;
 	static bool DownIndexTrigger => false;
+	static float ScaleChanger => 0f; 
 #endif
 
 	void Start()
@@ -57,12 +68,23 @@ public class PrefabTester : MonoBehaviour
 		}
 	}
 
+	void FixedUpdate()
+	{
+		if (Mathf.Abs(ScaleChanger) > 0.2f)
+		{
+			Scale += ScaleChanger * 0.01f;
+			if (currentInstance) currentInstance.transform.localScale = initialScale * Scale;
+		}
+	}
+
 	void SpawnPrefab(int index)
 	{
 		if (prefabs.Length == 0) throw new UnityException("Set prefabs on the inspector.");
 
 		if (currentInstance) Destroy(currentInstance);
 		currentInstance = Instantiate(prefabs[index], transform.position, Quaternion.identity);
+		initialScale = currentInstance.transform.localScale;
+		currentInstance.transform.localScale = initialScale * Scale;
 		spawned = true;
 	}
 
